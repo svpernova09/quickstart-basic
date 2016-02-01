d6e3e73995ce53002506c49f2e9ed60036781ce9
Joe Ferguson <joe@joeferguson.me>
2016-01-31 18:11:30 -0600

    Add users in similar fashion to how we added tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,7 @@
 */
 
 use App\Task;
+use App\User;
 use Illuminate\Http\Request;
 
 /**
@@ -19,7 +20,8 @@ use Illuminate\Http\Request;
  */
 Route::get('/', function () {
     return view('tasks', [
-    	'tasks' => Task::orderBy('created_at', 'asc')->get()
+    	'tasks' => Task::orderBy('created_at', 'asc')->get(),
+    	'users' => User::orderBy('created_at', 'asc')->get(),
     ]);
 });
 
@@ -51,6 +53,38 @@ Route::post('/task', function (Request $request) {
  */
 Route::delete('/task/{id}', function ($id) {
 	Task::findOrFail($id)->delete();
+
+	return redirect('/');
+});
+
+/**
+ * Add New User
+ */
+Route::post('/user', function (Request $request) {
+	$validator = Validator::make($request->all(), [
+		'name' => 'required|max:255',
+		'email' => 'required|email',
+	]);
+
+	if ($validator->fails()) {
+		return redirect('/')
+			->withInput()
+			->withErrors($validator);
+	}
+
+	$user = new User;
+	$user->name = $request->name;
+	$user->email = $request->email;
+	$user->save();
+
+	return redirect('/');
+});
+
+/**
+ * Delete User
+ */
+Route::delete('/user/{id}', function ($id) {
+	User::findOrFail($id)->delete();
 
 	return redirect('/');
 });
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -37,6 +37,49 @@
 				</div>
 			</div>
 
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					New User
+				</div>
+
+				<div class="panel-body">
+					<!-- Display Validation Errors -->
+					@include('common.errors')
+
+							<!-- New User Form -->
+					<form action="/user" method="POST" class="form-horizontal">
+						{{ csrf_field() }}
+
+						<!-- User Name -->
+						<div class="form-group">
+							<label for="user-name" class="col-sm-3 control-label">User Name</label>
+
+							<div class="col-sm-6">
+								<input type="text" name="name" id="user-name" class="form-control" value="{{ old('user') }}">
+							</div>
+						</div>
+
+						<!-- User Name -->
+						<div class="form-group">
+							<label for="user-name" class="col-sm-3 control-label">User Email</label>
+
+							<div class="col-sm-6">
+								<input type="text" name="email" id="user-email" class="form-control" value="{{ old('email') }}">
+							</div>
+						</div>
+
+						<!-- Add User Button -->
+						<div class="form-group">
+							<div class="col-sm-offset-3 col-sm-6">
+								<button type="submit" class="btn btn-default">
+									<i class="fa fa-plus"></i>Add User
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+
 			<!-- Current Tasks -->
 			@if (count($tasks) > 0)
 				<div class="panel panel-default">
@@ -73,6 +116,44 @@
 					</div>
 				</div>
 			@endif
+					<!-- Current Users -->
+				@if (count($users) > 0)
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							Current Users
+						</div>
+
+						<div class="panel-body">
+							<table class="table table-striped user-table">
+								<thead>
+								<th>Name</th>
+								<th>Email</th>
+								<th>&nbsp;</th>
+								</thead>
+								<tbody>
+								@foreach ($users as $user)
+									<tr>
+										<td class="table-text"><div>{{ $user->name }}</div></td>
+										<td class="table-text"><div>{{ $user->email }}</div></td>
+
+										<!-- User Delete Button -->
+										<td>
+											<form action="/user/{{ $user->id }}" method="POST">
+												{{ csrf_field() }}
+												{{ method_field('DELETE') }}
+
+												<button type="submit" class="btn btn-danger">
+													<i class="fa fa-trash"></i>Delete
+												</button>
+											</form>
+										</td>
+									</tr>
+								@endforeach
+								</tbody>
+							</table>
+						</div>
+					</div>
+				@endif
 		</div>
 	</div>
 @endsection
